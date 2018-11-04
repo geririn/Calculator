9420d4c92f2ad24b4a7fad98acc162596b385d3f
geririn <geririn@mail.ru>
2018-11-04 18:13:34 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public static class Calculator
+    {
+        public static decimal Sum(decimal firstAddend, decimal secondAddend)
+        {
+            return firstAddend + secondAddend;
+        }
+
+        public static decimal Sub(decimal minuend, decimal subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
+        public static decimal Mul(decimal firstMultiply, decimal secondMultiply)
+        {
+            return firstMultiply * secondMultiply;
+        }
+
+        public static decimal Div(decimal dividend, decimal devider)
+        {
+            return dividend / devider;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        private static bool _continue = true; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калькулятор может производить вычисления: сложение +, вычитание -, умножение *, деление /");
+            while(_continue)
+            {
+                Console.Write("Выберете оператора: ");
+                var oper = Console.ReadLine();
+                Console.Write("Введите первый аргумент: ");
+                var firstArgument = decimal.Parse(Console.ReadLine());
+                Console.Write("Введите второй аргумент: ");
+                var secondArgument = decimal.Parse(Console.ReadLine());
+                if (oper=="+")
+                {
+                    Console.WriteLine("Результат: {0}", Calculator.Sum(firstArgument, secondArgument));
+                    
+                }
+
+                if (oper=="-")
+                {
+                    Console.WriteLine("Результат: {0}", Calculator.Sub(firstArgument, secondArgument));
+                }
+
+                if (oper=="*")
+                {
+                    Console.WriteLine("Результат: {0}", Calculator.Mul(firstArgument, secondArgument));
+                }
+
+                if (oper=="/")
+                {
+                    if (secondArgument==0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Результат: {0}", Calculator.Div(firstArgument, secondArgument));
+                    }
+                }
+                Console.WriteLine("Продолжить? (Y/N)");
+                var exitChoice = Console.ReadLine();
+                if (exitChoice=="N")
+                {
+                    _continue = false;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
